824e2ae440c691c19f0d5dc275442791e6b31031
Juan Pablo Leiva <juampi.leiva5@gmail,com>
2026-06-24 09:51:18 -0300

    Ajusta estetica de publicaciones sprint 3

--- frontend/src/app/components/publicacion-card/publicacion-card.component.ts
+++ frontend/src/app/components/publicacion-card/publicacion-card.component.ts
@@ -1,18 +1,23 @@
-import { CommonModule } from '@angular/common';
+﻿import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { RouterLink } from '@angular/router';
 import { Publicacion } from '../../services/publicaciones.service';
 
 @Component({
   selector: 'app-publicacion-card',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule],
   templateUrl: './publicacion-card.component.html',
   styleUrl: './publicacion-card.component.css',
 })
 export class PublicacionCardComponent {
   @Input({ required: true }) publicacion!: Publicacion;
   @Input() mostrandoPerfil = false;
+
   @Output() cambiarLike = new EventEmitter<Publicacion>();
   @Output() eliminar = new EventEmitter<Publicacion>();
+
+  ocultarImagen(event: Event) {
+    const imagen = event.target as HTMLImageElement;
+    imagen.style.display = 'none';
+  }
 }
